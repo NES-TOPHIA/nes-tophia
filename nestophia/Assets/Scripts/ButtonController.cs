7b6a67a56aac73a562d7ddbe61ed8c835e12e74f
MinJu Kim <ssssssun56@sookmyung.ac.kr>
2025-01-31 20:29:44 +0900

    add button icon

--- nestophia/Assets/Scripts/ButtonController.cs
+++ nestophia/Assets/Scripts/ButtonController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class ButtonController : MonoBehaviour
 {
     public GameObject newPostPanel;
+    public GameObject settingPowerBtn;
 
     public void ClickCancelPostBtn()
     {
@@ -15,5 +16,11 @@ public class ButtonController : MonoBehaviour
         Debug.Log("Upload Post Button Clicked");
         newPostPanel.SetActive(false);
         // 여기에 게시물 업로드 코드 작성 or 다른 스크립트 생성
+    }
+
+    public void ClickMenuBtn()
+    {
+        Debug.Log("Menu Button Clicked");
+        settingPowerBtn.SetActive(!settingPowerBtn.activeSelf);
     }
 }
