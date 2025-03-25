6fd4cf146c60a88f5157bf42fff81bad1bc34d95
dong0one <dong0one9659@sookmyung.ac.kr>
2025-03-25 19:12:55 +0900

    startHost & startClient 버튼 추가

--- nestophia/Assets/Scripts/VRButtonManager.cs
+++ nestophia/Assets/Scripts/VRButtonManager.cs
@@ -1,9 +1,14 @@
+using Unity.Netcode;
 using UnityEngine;
-
+using System.Net;
+using System.Net.Sockets;
+using TMPro;
 
 public class VRButtonManager : MonoBehaviour
 {
     public Canvas searchCanvas;
+    public TextMeshProUGUI logText1;
+    public TextMeshProUGUI logText2;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     
@@ -15,5 +20,19 @@ public class VRButtonManager : MonoBehaviour
     public void CloseCanvas()
     {
         searchCanvas.gameObject.SetActive(false);
+    }
+
+    public void StartHost()
+    {
+        NetworkManager.Singleton.StartHost(); 
+        logText1.text = "Start Host";
+        logText2.text = "Start Host";
+    }
+
+    public void StartClient()
+    {
+        NetworkManager.Singleton.StartClient();
+        logText1.text = "StartClient";
+        logText2.text = "StartClient";
     }
 }
