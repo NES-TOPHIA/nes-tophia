6e9f4af41b885ee7a1bb8256ec42c8f683875f4b
dong0one <dong0one9659@sookmyung.ac.kr>
2025-05-14 23:53:01 +0900

    프리팹 추가

--- nestophia/Assets/Scripts/Elevator/PlayerMovement.cs
+++ nestophia/Assets/Scripts/Elevator/PlayerMovement.cs
@@ -24,11 +24,6 @@ public class PlayerMoveController : MonoBehaviour
     
     void Awake()
     {
-        if (instance != null && instance != this)
-        {
-            Destroy(gameObject);
-            return;
-        }
 
         instance = this;
         DontDestroyOnLoad(gameObject);
